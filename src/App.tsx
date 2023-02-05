d3a3339f70c4d96fb6279ebfb2f11a8935f4fef9
DjFIREED <porebskifilip@wp.pl>
2023-02-05 19:50:30 +0100

    removed styles

--- src/App.tsx
+++ src/App.tsx
@@ -9,27 +9,7 @@ function App() {
   const profile = useAppSelector((state) => state.profile);
   const dispatch = useAppDispatch();
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div style={{ width: "100vw", height: "100vh" }}>
       <div onClick={() => dispatch(logIn())}>LogIn</div>
     </div>
   );
