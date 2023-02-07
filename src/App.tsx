588a9970458aece02311c184aeffc5d472cdb236
DjFIREED <porebskifilip@wp.pl>
2023-02-07 10:46:47 +0100

    apptsx remove unused things

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import React from "react";
 import "./App.css";
-import { useAppSelector, useAppDispatch } from "./hooks/reduxHooks";
-import { logIn } from "./redux/profileSlice";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const profile = useAppSelector((state) => state.profile);
-  const dispatch = useAppDispatch();
-  return (
-    <div style={{ width: "100vw", height: "100vh" }}>
-      <div onClick={() => dispatch(logIn())}>LogIn</div>
-    </div>
-  );
+  return <div style={{ width: "100vw", height: "100vh" }}></div>;
 }
 
 export default App;
