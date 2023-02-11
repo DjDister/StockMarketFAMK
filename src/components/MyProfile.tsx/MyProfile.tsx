fa5491671290dc57f5dabdff72cc7835f6ff1a55
DjFIREED <porebskifilip@wp.pl>
2023-02-11 10:39:02 +0100

    myprofile, footer fix

--- src/components/MyProfile.tsx/MyProfile.tsx
+++ src/components/MyProfile.tsx/MyProfile.tsx
@@ -2,12 +2,19 @@ import React from "react";
 import Edit2 from "../../assets/icons/Edit2";
 import Email from "../../assets/icons/Email";
 import Phone from "../../assets/icons/Phone";
+import Button from "../Button/Button";
 import Input from "../Input/Input";
 import styles from "./MyProfile.module.css";
 export default function MyProfile() {
   return (
     <div className={styles.container}>
-      <div className={styles.label}>My profile</div>
+      <div className={styles.label}>
+        My profile
+        <div className={styles.labelIcon}>
+          <Edit2 fill={"#7c7e8d"} />
+          Edit
+        </div>
+      </div>
       <div className={styles.informationContainer}>
         <div className={styles.informationLabel}>Personal Information</div>
         <div className={styles.inputsContainer}>
@@ -98,6 +105,19 @@ export default function MyProfile() {
               />
             </div>
           </div>
+        </div>
+      </div>
+      <div className={styles.confirmContainer}>
+        <div className={styles.createdAtLabel}>
+          This account was created on January 10,2022,2:12PM
+        </div>
+        <div className={styles.buttonsContainer}>
+          <Button flex className={styles.buttonCancel}>
+            Cancel
+          </Button>
+          <Button flex className={styles.buttonSave}>
+            Save Changes
+          </Button>
         </div>
       </div>
     </div>
