be4f55a0abea3ea39e5e651c00c053dfdf8bdeaa
Aleksy <psychologianauki@gmail.com>
2023-02-16 20:41:43 +0100

    Created MarketPage without functionality

--- src/components/Marketitem/Marketitem.tsx
+++ src/components/Marketitem/Marketitem.tsx
@@ -1,31 +1,34 @@
 import React from "react";
-import styles from "./Market.module.css";
+import styles from "../../pages/MarketPage/MarketPage.module.css";
 
 interface CryptoProps {
   icon?: JSX.Element;
   name: string;
   shortname: string;
   graph?: JSX.Element;
-  price?: string;
+  price?: number;
   changeprice?: string;
+  counter?: number;
 }
 
-export default function Elem({
+export default function Crypto({
   icon,
   name,
   shortname,
   graph,
   price,
   changeprice,
+  counter,
 }: CryptoProps) {
   return (
     <div className={styles.crypto}>
+      <div>{counter}</div>
       <div className={styles.icon}>{icon}</div>
       <div className={styles.nazwy}>
         <div className={styles.name}>{name}</div>
         <div className={styles.shortname}>{shortname}</div>
       </div>
-      <div className={styles.graph}>{graph}</div>
+      <div className={styles.graph}>{}</div>
       <div className={styles.overallprice}>
         <div className={styles.price}>{price}</div>
         <div className={styles.pricechanges}>{changeprice}</div>
---
+++ src/pages/MarketPage/MarketPage.tsx
@@ -0,0 +1,213 @@
+import React from "react";
+import styles from "./MarketPage.module.css";
+
+import { useState } from "react";
+import Layout from "../../components/Layout/Layout";
+import Button from "../../components/Button/Button";
+import Crypto from "../../components/Marketitem/Marketitem";
+import Input from "../../components/Input/Input";
+import Loupe from "../../assets/icons/Loupe";
+import Filter from "../../assets/icons/Filter";
+import Market1 from "../../assets/icons/Market1";
+import Market2 from "../../assets/icons/Market2";
+import Market3 from "../../assets/icons/Market3";
+import DownArrow from "../../assets/icons/DownArrow";
+import TriangleUp from "../../assets/icons/TriangleUp";
+const button = [
+  { name: "Category" },
+  { name: "Algorithm" },
+  { name: "Platform" },
+  { name: "Industry" },
+];
+
+const sortcrypto = [
+  { name: "Coin Name " },
+  { name: "Chart" },
+  { name: "Coin Price" },
+];
+const sortcryptoweb = [
+  { name: "#" },
+  { name: "Coin Name " },
+  { name: "Coin Price" },
+  { name: "24%" },
+  { name: "24h High" },
+  { name: "24h Low" },
+  { name: "Chart" },
+];
+const navbarcrypto = [
+  { name: "Top Gainers" },
+  { name: "Top Loses" },
+  { name: "New in market" },
+];
+const i = 1;
+const cryptolist = [
+  { name: "Bitcoin", shortname: "BTC", price: 20000, counter: "" },
+  { name: "Etherium", shortname: "ETH", price: 1500, counter: "" },
+  { name: "Skale", shortname: "SKL", price: 0.05, counter: "" },
+  { name: "Atom", shortname: "ATM", price: 14, counter: "" },
+];
+
+export default function MarketPage() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [isSortedByName, setIsSortedByName] = useState(false);
+  const [cryptoarray, setCryptoarray] = useState(cryptolist);
+  const [isSortedByPrice, setIsSortedByPrice] = useState(false);
+
+  const [sortedArr, setSortedArr] = useState(cryptolist);
+
+  console.log(isSortedByPrice);
+  console.log(isSortedByName);
+
+  return (
+    <Layout>
+      <div className={styles.Market}>
+        <div className={styles.part1}>
+          <div className={styles.linia1}>
+            <div className={styles.tytul}>
+              <span className={styles.title}>Market Coins</span>
+            </div>
+            <div className={styles.inputdiv}>
+              {<Loupe width="24px" fill="gray" />}
+              <Input
+                className={styles.inputtitle}
+                placeholder="Search Coin Name"
+                onChange={() => null}
+              />
+            </div>
+            <div className={styles.filter}>
+              {<Filter fill="none" width="25px" />}Filter
+            </div>
+
+            <div className={styles.container}>
+              <div className={styles.square}></div>
+              <div className={styles.square}></div>
+              <div className={styles.square}></div>
+            </div>
+          </div>
+          <div>
+            <p>jakis bardzo madry tekst ut bedzie</p>
+          </div>
+          <div className={styles.overallbuttons}>
+            <div className={styles.przyciski}>
+              {button.map((x) => (
+                <Button
+                  flex
+                  className={styles.przycisk}
+                  onClick={() => <div></div>}
+                >
+                  {x.name}
+                  {<DownArrow />}
+                </Button>
+              ))}
+            </div>
+            <div className={styles.przyciski2}>
+              <Button className={styles.show20}>
+                Show 20 <DownArrow />
+              </Button>
+              {<Market1 fill="white" height="38px" />}
+              {<Market2 fill="white" />}
+              {<Market3 fill="white" />}
+            </div>
+          </div>
+          <div className={styles.sortowanie}>
+            {sortcrypto.map((y, index) => (
+              <div
+                className={styles.sortbutton}
+                onClick={
+                  (index === 0
+                    ? () =>
+                        setSortedArr(
+                          cryptolist.sort((a, b) => {
+                            if (a.name < b.name) return -1;
+                            if (a.name > b.name) return 1;
+                            return 0;
+                          })
+                        )
+                    : undefined) ||
+                  (index === 2
+                    ? () =>
+                        setSortedArr(
+                          cryptolist.sort((a, b) => a.price - b.price)
+                        )
+                    : undefined)
+                }
+              >
+                {y.name}
+                {<TriangleUp fill="white" height="10px" />}
+              </div>
+            ))}
+          </div>
+          <div className={styles.sortowanie2}>
+            {sortcryptoweb.map((y) => (
+              <div className={styles.sortbutton}>
+                {y.name}
+                {<TriangleUp fill="white" height="10px" />}
+              </div>
+            ))}
+          </div>
+
+          <div className={styles.cryptocontainer}>
+            {sortedArr.map((x, index) => (
+              <Crypto
+                counter={index + 1}
+                name={x.name}
+                shortname={x.shortname}
+                price={x.price}
+              />
+            ))}
+
+            <div className={styles.buysellbutton}>
+              <Button className={styles.buyselltekst}>Buy & Sell</Button>
+            </div>
+          </div>
+          <div className={styles.last}>
+            <div>assets</div>
+            <div> 1... 2... 3...4</div>
+          </div>
+        </div>
+        <div className={styles.part2}>
+          <div className={styles.navbar}>
+            {" "}
+            <div className={styles.navbarcrypto}>
+              {navbarcrypto.map((y) => (
+                <div className={styles.navbarbutton}>{y.name}</div>
+              ))}
+            </div>
+          </div>
+          <div className={styles.sortowanie3}>
+            {sortcrypto.map((y) => (
+              <div className={styles.sortbutton}>
+                {y.name}
+                {<TriangleUp fill="white" height="10px" />}
+              </div>
+            ))}
+          </div>
+          <div className={styles.cryptocontainer}>
+            {cryptolist.map((x) => (
+              <Crypto name={x.name} shortname={x.shortname} price={x.price} />
+            ))}
+          </div>
+          <div className={styles.last}>
+            <div>assets</div>
+            <div> 1... 2... 3...4</div>
+          </div>
+
+          <div className={styles.watchlist}>Watchlist</div>
+          <div className={styles.sortowanie3}>
+            {sortcrypto.map((y) => (
+              <div className={styles.sortbutton}>
+                {y.name}
+                {<TriangleUp fill="white" height="10px" />}
+              </div>
+            ))}
+          </div>
+          <div className={styles.cryptocontainer}>
+            {cryptolist.map((x) => (
+              <Crypto name={x.name} shortname={x.shortname} price={x.price} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
