b9be367af50fbe32dab7c89c5f4176d515bc0d19
DjFIREED <porebskifilip@wp.pl>
2023-02-19 08:05:30 +0100

    calculating portfolio

--- src/pages/PortfolioPage/PortfolioPage.tsx
+++ src/pages/PortfolioPage/PortfolioPage.tsx
@@ -1,5 +1,5 @@
-import { doc, updateDoc } from "@firebase/firestore";
-import React from "react";
+import { doc, getDoc, updateDoc } from "@firebase/firestore";
+import React, { useEffect, useState } from "react";
 import db from "../../../firebaseConfig";
 import CardRight from "../../assets/icons/CardRight";
 import EyeOff from "../../assets/icons/EyeOff";
@@ -8,7 +8,10 @@ import Wallet from "../../assets/icons/Wallet";
 import DetailsCard from "../../components/DetailsCard/DetailsCard";
 import Layout from "../../components/Layout/Layout";
 import { useAppSelector } from "../../hooks/reduxHooks";
+import converter from "../../utils/converter";
 import styles from "./PortfolioPage.module.css";
+import { PortfolioType, UserData } from "../../types";
+import axios from "axios";
 
 export default function PortfolioPage() {
   const profile = useAppSelector((state) => state.profile);
@@ -29,9 +32,75 @@ export default function PortfolioPage() {
       },
     ];
     await updateDoc(doc(db, "users", profile.userId), {
-      portofolio: testCryptos,
+      portfolio: testCryptos,
     });
   };
+
+  const [userPortfolio, setUserPortfolio] = useState<PortfolioType | undefined>(
+    undefined
+  );
+
+  const [totalReturn, setTotalReturn] = useState<number | undefined>(undefined);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const docRef = doc(db, "users", profile.userId).withConverter(
+        converter<UserData>()
+      );
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        setUserPortfolio(docSnap.data().portfolio);
+        const data = docSnap.data().portfolio;
+        const totalReturn = await (
+          await Promise.all(
+            data.map(async (curr) => {
+              const response = await axios.request({
+                method: "GET",
+                baseURL: "https://rest.coinapi.io/v1",
+                url: `/exchangerate/${curr.symbol}/USD`,
+                headers: {
+                  "X-CoinAPI-Key": "976C4BA7-395F-4529-86B7-888A9F92493C",
+                },
+              });
+
+              const data = await response.data;
+              const currentPrice = data.rate;
+
+              return parseInt(curr.amount) * currentPrice;
+            })
+          )
+        ).reduce((acc, curr) => acc + curr, 0);
+        setTotalReturn(totalReturn);
+      }
+    };
+    fetchUserData();
+  }, []);
+
+  useEffect(() => {
+    async function fetchCryptocurrencies() {
+      const response = await axios.request({
+        method: "GET",
+        baseURL: "rest.coinapi.io",
+        url: `/v1/symbols`,
+        headers: {
+          "X-CoinAPI-Key": "0C937E64-E825-4D2D-8E19-2B245D6A87B7",
+        },
+      });
+
+      console.log(response);
+    }
+
+    fetchCryptocurrencies();
+  }, []);
+
+  const totalInvestment = userPortfolio?.reduce(
+    (acc, curr) => acc + parseInt(curr.boughtPrice) * parseInt(curr.amount),
+    0
+  );
+  const profitAmount =
+    totalReturn && totalInvestment
+      ? (totalReturn - totalInvestment).toFixed(2)
+      : 0;
   return (
     <Layout>
       <div
@@ -47,56 +116,64 @@ export default function PortfolioPage() {
         }}
       />
       <div className={styles.container}>
-        <div className={styles.portfolioSummaryContainer}>
-          <div className={styles.titleContainer}>
-            <div className={styles.labelCont}>
-              <div className={styles.title}>Portfolio</div>
-              <div className={styles.updateDate}>
-                Update 16/02/2022 at 2:30PM
-              </div>
-            </div>
-            <div>
-              <MoreVertical fill={"grey"} />
-            </div>
-          </div>
-          <div className={styles.detailsContainer}>
-            <div className={styles.availableCont}>
-              <div className={styles.availableTitle}>
-                <Wallet />
-                <div className={styles.avtitle}>Available Balance</div>
-              </div>
-              <div className={styles.avmoneyCont}>
-                <div className={styles.avamount}>32,444$</div>
-                <div className={styles.eyeIcon}>
-                  <EyeOff fill="grey" />
+        {userPortfolio ? (
+          <div className={styles.portfolioSummaryContainer}>
+            <div className={styles.titleContainer}>
+              <div className={styles.labelCont}>
+                <div className={styles.title}>Portfolio</div>
+                <div className={styles.updateDate}>
+                  Update 16/02/2022 at 2:30PM
                 </div>
               </div>
+              <div>
+                <MoreVertical fill={"grey"} />
+              </div>
             </div>
-            <div className={styles.potrfolioCardsCont}>
-              <div className={styles.twoCardsCont}>
+            <div className={styles.detailsContainer}>
+              <div className={styles.availableCont}>
+                <div className={styles.availableTitle}>
+                  <Wallet />
+                  <div className={styles.avtitle}>Available Balance</div>
+                </div>
+                <div className={styles.avmoneyCont}>
+                  <div className={styles.avamount}>32,444$</div>
+                  <div className={styles.eyeIcon}>
+                    <EyeOff fill="grey" />
+                  </div>
+                </div>
+              </div>
+              <div className={styles.potrfolioCardsCont}>
+                <div className={styles.twoCardsCont}>
+                  <DetailsCard
+                    labelIcon={<CardRight />}
+                    labelText={"Total Investment"}
+                    customStyle={{ width: "50%" }}
+                    amount={`$${totalInvestment}`}
+                  />
+                  <DetailsCard
+                    labelIcon={<CardRight />}
+                    labelText={"Total return"}
+                    amount={`$${totalReturn ? totalReturn.toFixed(2) : 0}`}
+                    customStyle={{ width: "50%" }}
+                  />
+                </div>
                 <DetailsCard
+                  className={styles.thirdCard}
                   labelIcon={<CardRight />}
-                  labelText={"Total Investment"}
-                  customStyle={{ width: "50%" }}
-                  amount={"$32,444"}
-                />
-                <DetailsCard
-                  labelIcon={<CardRight />}
-                  labelText={"Total return"}
-                  amount={"$32,444"}
-                  customStyle={{ width: "50%" }}
+                  labelText={"Profit/Loss"}
+                  amount={`$${profitAmount}`}
+                  returnIndicator={
+                    profitAmount && totalInvestment
+                      ? (parseInt(profitAmount) / totalInvestment).toFixed(2)
+                      : 0
+                  }
                 />
               </div>
-              <DetailsCard
-                className={styles.thirdCard}
-                labelIcon={<CardRight />}
-                labelText={"Total Investment"}
-                amount={"$32,444"}
-                returnIndicator={-2}
-              />
             </div>
           </div>
-        </div>
+        ) : (
+          <div>Loading...</div>
+        )}
       </div>
     </Layout>
   );
