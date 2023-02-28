4536434d8cda4f6f38752ab7687718c66023d6ff
Aleksy <psychologianauki@gmail.com>
2023-02-28 21:10:38 +0100

    styling, dataBase request, buyCrypto functionality

--- src/pages/CryptoItemPage/CryptoItemPage.tsx
+++ src/pages/CryptoItemPage/CryptoItemPage.tsx
@@ -11,9 +11,15 @@ import TrendingDown from "../../assets/icons/TrendingDown";
 import TrendingUp from "../../assets/icons/TrendingUp";
 import Info from "../../assets/icons/Info";
 import UpDown from "../../assets/icons/UpDown";
-import { getFirestore } from "firebase/firestore";
-
+import db from "../../../firebaseConfig";
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { useAppSelector } from "../../hooks/reduxHooks";
 import Star from "../../assets/icons/Star";
+import { async } from "@firebase/util";
+import { update } from "plotly.js";
+import Portfolio from "../../assets/icons/Portfolio";
+import { updateDoc } from "firebase/firestore";
+import { arrayUnion } from "firebase/firestore";
 export default function CryptoItemPage() {
   interface CryptoItemType {
     id: string;
@@ -45,6 +51,43 @@ export default function CryptoItemPage() {
   const [low_24h, setLow_24h] = useState(10);
   const [current_price, SetCurrent_price] = useState(10);
   const [market_cup_rank, setMarket_cap_rank] = useState(10);
+  const [data2, setData2] = useState<CryptoItemType | undefined>(undefined);
+  const [inputValue, setInputValue] = useState<string>("");
+  const [wallet, setWallet] = useState<string>("");
+  const user = useAppSelector((state) => state.profile);
+
+  console.log(user);
+  const handleButtonClick = async () => {
+    const inputValueToSave = inputValue;
+
+    if (inputValueToSave !== "") {
+      console.log(`Input value "${inputValueToSave}" saved.`);
+    }
+
+    if (
+      inputValue !== "" &&
+      data2?.market_data.current_price?.usd &&
+      inputValue
+    ) {
+      const toDataBase = {
+        amount: parseInt(inputValue) / data2?.market_data.current_price?.usd,
+        boughtPrice: data2.market_data.current_price.usd,
+        name: data2.name,
+        symbol: data2.symbol,
+      };
+      await updateDoc(doc(db, "users", user.userId), {
+        portfolio: arrayUnion(toDataBase),
+      });
+      const walletState = parseInt(wallet) - parseInt(inputValue);
+      console.log(walletState);
+      await updateDoc(doc(db, "users", user.userId), {
+        wallet: { totalBalanceDollars: walletState.toString() },
+      });
+    }
+  };
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setInputValue(event.target.value);
+  };
 
   useEffect(() => {
     const fetchHistoricData = async () => {
@@ -53,12 +96,15 @@ export default function CryptoItemPage() {
       );
       console.log(data);
       setHistoricData(data.prices);
+      const dataFromDb = await getDoc(doc(db, "users", user.userId));
+      console.log(dataFromDb.data());
+      if (dataFromDb.exists()) {
+        setWallet(dataFromDb.data().wallet.totalBalanceDollars);
+      }
+      console.log(wallet);
     };
     fetchHistoricData();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  const [data2, setData2] = useState<CryptoItemType | undefined>(undefined);
-  useEffect(() => {
+
     async function fetchData() {
       const response = await axios.get(
         `https://api.coingecko.com/api/v3/coins/${id}`
@@ -82,151 +128,199 @@ export default function CryptoItemPage() {
   }, []);
 
   return (
-    <div className={styles.overall}>
-      <div className={styles.container}>
-        <div className={styles.cryptoTitleContainer}>
-          <div className={styles.widthSet}>
-            <div className={styles.marketStats}>Market stats </div>
-            <div className={styles.partdivide}>
-              <div className={styles.part1}>
-                <div className={styles.cryptoName}>
-                  <div className={styles.symbolName}>
-                    <div>
-                      <img src={data2?.image?.small}></img>
-                    </div>
-                    <div className={styles.querySymbolName}>
-                      <div>{data2 && data2.name}</div>
-                      <div
-                        className={styles.symbol}
-                        style={{ color: "color: rgb(54, 54, 153)" }}
-                      >
-                        {data2?.symbol}
+    <Layout>
+      <div className={styles.overall}>
+        <div className={styles.container}>
+          <div className={styles.part1}>
+            <div className={styles.cryptoTitleContainer}>
+              <div className={styles.widthSet}>
+                <div className={styles.marketStats}>Market stats </div>
+                <div className={styles.partdivide}>
+                  <div className={styles.part1}>
+                    <div className={styles.cryptoName}>
+                      <div className={styles.symbolName}>
+                        <div>
+                          <img src={data2?.image?.small}></img>
+                        </div>
+                        <div className={styles.querySymbolName}>
+                          <div>{data2 && data2.name}</div>
+                          <div
+                            className={styles.symbol}
+                            style={{ color: "color: rgb(54, 54, 153)" }}
+                          >
+                            {data2?.symbol}
+                          </div>
+                        </div>
+                      </div>
+                      <div style={{ height: "30px", backgroundColor: "black" }}>
+                        {
+                          <Star
+                            fill={fav ? "color: rgb(54, 54, 153)" : "gray"}
+                          />
+                        }
                       </div>
                     </div>
-                  </div>
-                  <div style={{ height: "30px", backgroundColor: "black" }}>
-                    {<Star fill={fav ? "color: rgb(54, 54, 153)" : "gray"} />}
-                  </div>
-                </div>
-                <div className={styles.cryptoDetailsContainer}>
-                  <div
-                    className={styles.cryptoDetail}
-                    style={{ backgroundColor: "black", borderRadius: "5px" }}
-                  >
-                    Rank #{data2?.market_data?.market_cap_rank}
-                  </div>
-                  <div className={styles.cryptoDetail}>Coin</div>
-                  <div className={styles.cryptoDetail}>On 1 watchlists </div>
-                </div>
-              </div>
-              <div className={styles.part2}>
-                <div className={styles.priceContainer}>
-                  <div className={styles.price}>
-                    {data2?.market_data?.current_price.usd}{" "}
-                    {data2?.market_data?.price_change_percentage_24h && (
+                    <div className={styles.cryptoDetailsContainer}>
                       <div
+                        className={styles.cryptoDetail}
                         style={{
-                          display: "flex",
-                          alignItems: "center",
-                          color:
-                            data2?.market_data?.price_change_percentage_24h > 0
-                              ? "#039763"
-                              : data2?.market_data
-                                  ?.price_change_percentage_24h === 0
-                              ? "grey"
-                              : "red",
+                          backgroundColor: "black",
+                          borderRadius: "5px",
                         }}
                       >
-                        {data2?.market_data?.price_change_percentage_24h}%
-                        {data2?.market_data?.price_change_percentage_24h >=
-                        0 ? (
-                          <TrendingUp
-                            fill={
-                              price_change_percentage_24h > 0 ? "green" : "grey"
-                            }
-                          />
-                        ) : (
-                          <TrendingDown fill="red" />
+                        Rank #{data2?.market_data?.market_cap_rank}
+                      </div>
+                      <div className={styles.cryptoDetail}>Coin</div>
+                      <div className={styles.cryptoDetail}>
+                        On 1 watchlists{" "}
+                      </div>
+                    </div>
+                  </div>
+                  <div className={styles.part2}>
+                    <div className={styles.priceContainer}>
+                      <div className={styles.price}>
+                        {data2?.market_data?.current_price.usd}{" "}
+                        {data2?.market_data?.price_change_percentage_24h && (
+                          <div
+                            style={{
+                              display: "flex",
+                              alignItems: "center",
+                              color:
+                                data2?.market_data
+                                  ?.price_change_percentage_24h > 0
+                                  ? "#039763"
+                                  : data2?.market_data
+                                      ?.price_change_percentage_24h === 0
+                                  ? "grey"
+                                  : "red",
+                            }}
+                          >
+                            {data2?.market_data?.price_change_percentage_24h}%
+                            {data2?.market_data?.price_change_percentage_24h >=
+                            0 ? (
+                              <TrendingUp
+                                fill={
+                                  price_change_percentage_24h > 0
+                                    ? "green"
+                                    : "grey"
+                                }
+                              />
+                            ) : (
+                              <TrendingDown fill="red" />
+                            )}
+                          </div>
                         )}
                       </div>
-                    )}
-                  </div>
-                  <div className={styles.priceInfo}>
-                    {data2 && data2.name} Price
-                    {<Info fill="gray" width="20px" height="20px" />}
+                      <div className={styles.priceInfo}>
+                        {data2 && data2.name} Price
+                        {<Info fill="gray" width="20px" height="20px" />}
+                      </div>
+                    </div>
+                    <div className={styles.highLowContainer}>
+                      <div className={styles.highLowPrice24h}>
+                        <div
+                          className={styles.highlowprice}
+                          style={{ color: "gray" }}
+                        >
+                          {<UpDown width="20px" fill="white" />}High/Low Price
+                        </div>
+                        <div
+                          className={styles.timeFrame}
+                          style={{ color: "gray" }}
+                        >
+                          24h
+                          {
+                            <DownArrow
+                              fill="white"
+                              height="20px"
+                              width="20px"
+                            />
+                          }
+                        </div>
+                      </div>
+                      <div className={styles.line}>
+                        <div
+                          className={styles.blueLine}
+                          style={{
+                            width: `${
+                              (data2?.market_data?.high_24h?.usd &&
+                              data2?.market_data?.low_24h?.usd &&
+                              data2?.market_data?.current_price.usd
+                                ? (data2?.market_data?.current_price.usd -
+                                    data2?.market_data?.low_24h?.usd) /
+                                  (data2?.market_data?.high_24h?.usd -
+                                    data2?.market_data?.low_24h?.usd)
+                                : 40) * 100
+                            }%`,
+                          }}
+                        ></div>
+                      </div>
+                      <div className={styles.underLinePrices}>
+                        <div
+                          className={styles.lowPrice}
+                          style={{ color: "gray" }}
+                        >
+                          Low: {data2?.market_data?.low_24h?.usd}
+                        </div>
+                        <div
+                          className={styles.highPrice}
+                          style={{ color: "gray" }}
+                        >
+                          High: {data2?.market_data?.high_24h?.usd}
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
-                <div className={styles.highLowContainer}>
-                  <div className={styles.highLowPrice24h}>
-                    <div
-                      className={styles.highlowprice}
-                      style={{ color: "gray" }}
-                    >
-                      {<UpDown width="20px" fill="white" />}High/Low Price
-                    </div>
-                    <div className={styles.timeFrame} style={{ color: "gray" }}>
-                      24h{<DownArrow fill="white" height="20px" width="20px" />}
-                    </div>
-                  </div>
-                  <div className={styles.line}>
-                    <div
-                      className={styles.blueLine}
-                      style={{
-                        width: `${
-                          (data2?.market_data?.high_24h?.usd &&
-                          data2?.market_data?.low_24h?.usd &&
-                          data2?.market_data?.current_price.usd
-                            ? (data2?.market_data?.current_price.usd -
-                                data2?.market_data?.low_24h?.usd) /
-                              (data2?.market_data?.high_24h?.usd -
-                                data2?.market_data?.low_24h?.usd)
-                            : 40) * 100
-                        }%`,
-                      }}
-                    ></div>
-                  </div>
-                  <div className={styles.underLinePrices}>
-                    <div className={styles.lowPrice} style={{ color: "gray" }}>
-                      Low: {data2?.market_data?.low_24h?.usd}
-                    </div>
-                    <div className={styles.highPrice} style={{ color: "gray" }}>
-                      High: {data2?.market_data?.high_24h?.usd}
-                    </div>
-                  </div>
-                </div>
+                <div className={styles.components}>Components will be here</div>
               </div>
             </div>
-            <div className={styles.components}>Components will be here</div>
+            <div className={styles.graph} style={{ width: "100%" }}>
+              <>
+                {historicData ? (
+                  <Plot
+                    data={[
+                      {
+                        x: historicData.map((item: any) => {
+                          const date = new Date(item[0]);
+                          return date;
+                        }),
+                        y: historicData.map((item: any) => {
+                          return item[1];
+                        }),
+                        type: "scatter",
+                        mode: "lines+markers",
+                        marker: { color: "red" },
+                      },
+                    ]}
+                    layout={{
+                      width: 500,
+                      height: 500,
+                      title: `A ${data2 && data2.name} chart`,
+                    }}
+                  />
+                ) : null}
+              </>
+            </div>
+          </div>
+          <div className={styles.inputbuttondiv}>
+            <input
+              placeholder="$ to invest"
+              className={styles.inputdiv}
+              value={inputValue}
+              onChange={handleInputChange}
+            ></input>
+            <button
+              className={styles.buttondiv}
+              placeholder="buy"
+              style={{ width: "80px", height: "30px" }}
+              onClick={handleButtonClick}
+            >
+              BUY
+            </button>
           </div>
         </div>
-        <div className={styles.graph} style={{ width: "100%" }}>
-          <>
-            {historicData ? (
-              <Plot
-                data={[
-                  {
-                    x: historicData.map((item: any) => {
-                      const date = new Date(item[0]);
-                      return date;
-                    }),
-                    y: historicData.map((item: any) => {
-                      return item[1];
-                    }),
-                    type: "scatter",
-                    mode: "lines+markers",
-                    marker: { color: "red" },
-                  },
-                ]}
-                layout={{
-                  width: 1000,
-                  height: 1000,
-                  title: `A ${data2 && data2.name} chart`,
-                }}
-              />
-            ) : null}
-          </>
-        </div>
       </div>
-    </div>
+    </Layout>
   );
 }
