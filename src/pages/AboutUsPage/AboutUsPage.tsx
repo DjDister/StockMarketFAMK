91c09a07e952e2b65bd78b9da504b6e6ac24f04a
DjFIREED <porebskifilip@wp.pl>
2023-03-07 06:56:15 +0100

    images moved, filip card fix

--- src/pages/AboutUsPage/AboutUsPage.tsx
+++ src/pages/AboutUsPage/AboutUsPage.tsx
@@ -1,10 +1,9 @@
 import React from "react";
 import styles from "./AboutUsPage.module.css";
 import MyComponent from "./TeamMember";
-import zdjecie from "./images/zdjecie.jpg";
-import filip from "./images/filip.jpg";
-import krystian from "./images/krystian.jpg";
-import aleksy from "./images/aleksy.jpg";
+import filip from "../../assets/images/filip.jpg";
+import aleksy from "../../assets/images/aleksy.jpg";
+import krystian from "../../assets/images/krystian.jpg";
 import Layout from "../../components/Layout/Layout";
 export default function AboutUsPage() {
   return (
@@ -17,17 +16,24 @@ export default function AboutUsPage() {
             <MyComponent
               image={filip}
               imie="Filip Porębski"
-              opis="Niestety nie wszystkim studentom udało się uzyskać wymagane zaliczenia w letniej sesji. 
-          Niektórzy sami sobie zawinili, a innym zabrakło trochę szczęścia . Teraz muszą to nadrobić. 
-          Podpowiadamy jak poradzić sobie z drugim podejściem do egzaminu.
-          
-          We wrześniu wciąż jeszcze nie ma zajęć na uczelniach.
-          Jednak często musimy przypomnieć sobie gdzie leżą ich książki i notatki.
-          Powodem jest sesja poprawkowa. Różne są powody oblania egzaminu lub co gorsze egzaminów. 
-          Jedni musieli się przygotować tylko do dwóch, a inni nawet do pięciu.
-          Czasami wystarczy mniej więcej orientować się w zagadnieniach, 
-          gdy innym razem obkucie się ze wszystkich zagadnień może nie dać pozytywnej oceny."
-              email="studentdebil1@gmail.com"
+              opis="
+              I led a team of two friends in creating a
+               stock market project. I focused on keeping
+                everyone on track while I developed key pages
+                 like Profile, Market, Portfolio, Wallet, Register,
+                  and Login. I also implemented a complex app logic
+                   to make everything function smoothly, 
+                   like integrating APIs
+                    for real-time crypto market data, setup redux for whole app
+                     and secure user authentication processes. With our platform,
+                      users can view and edit their personal information,
+                       search and view crypto market data,
+                        track their investments, manage their
+                         account balances, and securely
+                          log in to the platform.
+                           Overall, I'm proud of my work and my team's dedication in making this project a success.
+              "
+              email="porebskifilip@wp.pl"
             />
             <MyComponent
               image={aleksy}
