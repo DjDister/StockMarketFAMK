331726f0f90a227e0573a9e41689b4248702007a
Aleksy <psychologianauki@gmail.com>
2023-03-08 16:10:07 +0100

    -ed

--- src/pages/AboutUsPage/AboutUsPage.tsx
+++ src/pages/AboutUsPage/AboutUsPage.tsx
@@ -43,7 +43,7 @@ export default function AboutUsPage() {
                cryptoItemPage and Market including setting and integrating API, created pagination,
                linked a user's long and short positions to a database,
                sorting functions, general styling and all necessary steps to make this website work. 
-               As the team we were in touch 24/7, develop and discussed new solutions together.
+               As the team we were in touch 24/7, developed and discussed new solutions together.
                 Personally I learnt a lot about TypeScript,
                  improved the quality of my code and discovered many possibilities offered by the frontend,
                   which will certainly be useful to me in the future. "
